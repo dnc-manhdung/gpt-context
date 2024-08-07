9757d1f6764918869ec8a8535d4b4cbd2dba1689
Mona Lisa <dungtm@missingcorner.com>
2024-08-07 18:14:43 +0700

    feat: chat page layout

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import DefaultLayout from '~/components/layouts/default'
+
+const Page = () => {
+  return <DefaultLayout>Hello</DefaultLayout>
+}
+
+export default Page
---
+++ src/components/layouts/navbar.tsx
@@ -0,0 +1,35 @@
+import { Flex, Text, Button } from '@mantine/core'
+
+type Chat = {
+  id: string
+  name: string
+}
+
+interface NavbarProps {
+  chatList: Chat[]
+}
+
+const Navbar: React.FC<NavbarProps> = ({ chatList }) => {
+  return (
+    <Flex direction="column" mt="md" ml="sm" mr="sm">
+      <Text className="text-sm text-gray-600">Recent Chat</Text>
+      <Flex direction="column" className="w-[200px]" gap="8" mt="md">
+        {chatList &&
+          chatList.map((chat) => {
+            return (
+              <Text
+                key={chat.id}
+                component="a"
+                className="text-md hover:bg-gray-100 px-1 py-1.5 rounded-md"
+                href={`/chat/${chat.id}`}
+              >
+                {chat.name}
+              </Text>
+            )
+          })}
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Navbar
