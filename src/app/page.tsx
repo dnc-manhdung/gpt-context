327028a0f30cc4c5afa39dd4bdb42abea2682eed
Mona Lisa <dungtm@missingcorner.com>
2024-08-07 10:51:35 +0700

    config mantine

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Button, Checkbox, Group, TextInput } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import Demo from './components/demo'
+
+export default function Page() {
+  return <Demo />
+}
