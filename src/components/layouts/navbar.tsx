83135b40c1c0c1566fdebd9434e4ea50bc95cd8e
gdschanu-manhdung <manhdungtran.vnuuet@gmail.com>
2024-08-13 14:12:29 +0700

    fix: using conversation bg

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text, Button } from '@mantine/core'
+import { useParams } from 'next/navigation'
 
 type Chat = {
   id: string
@@ -10,13 +11,23 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ chatList }) => {
+  const params = useParams()
+  const { id } = params
+
   return (
     <Flex direction="column" mt="md" ml="sm" mr="sm">
       <Text className="text-sm text-gray-600">Recent Chat</Text>
       <Flex direction="column" className="w-full" gap="8" mt="md">
         {chatList &&
           chatList.map((chat) => {
-            return (
+            return chat.id === id ? (
+              <Text
+                key={chat.id}
+                className="text-md px-1 py-1.5 bg-gray-200 rounded-md"
+              >
+                {chat.name}
+              </Text>
+            ) : (
               <Text
                 key={chat.id}
                 component="a"
